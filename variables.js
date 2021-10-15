06ecd5074228f233aa7bc7c543172accce36eb97
Trenton Buckley <trentb.buckley@gmail.comtrentb.buckley@gmail.comqq>
2021-10-15 09:33:06 -0600

    Finished variables.js

--- variables.js
+++ variables.js
@@ -2,16 +2,28 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
+console.log(`Problem 1`);
+
+let myName = `Trenton Buckley`;
+console.log(myName);
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
+console.log(`Problem 2`);
+
+let faveNum = 14;
+console.log(faveNum);
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
+console.log(`Problem 3`);
+
+let lovesCode = true;
+console.log(lovesCode);
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -21,10 +33,21 @@
 */
 
 //CODE HERE
+console.log(`Problem 4`);
 
+if(faveNum === 13) {
+  console.log(`Lucky 13!`);
+} else {
+  console.log(`Not Lucky 13!`);
+};
 //////////////////PROBLEM 5////////////////////
 /*
   Write a for loop that prints out your faveNum 5 times.
 */
 
 //CODE HERE
+console.log(`Problem 5`);
+
+for(let i = 0; i < 5; i++) {
+  console.log(faveNum);
+};
